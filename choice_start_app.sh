82ff0cd3ce95a3388befe37f942ab5cab57ee2b0
950654 <Mike.Lake@uts.edu.au>
2014-02-11 16:33:27 +1100

    Update to use systemd instead of SysV.

--- choice_start_app.sh
+++ choice_start_app.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo service httpd stop
-sudo service nginx start
+# Old SysV init methos
+#sudo service httpd stop
+#sudo service nginx start
+
+# New dystewmd method. 
+sudo systemctl stop httpd.service
+sudo systemctl start nginx.service
+
 ./choice.py
+
 
