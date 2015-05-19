71a3922d31f88bc8fcf65e4a2de029c98d5bc205
950654 <Mike.Lake@uts.edu.au>
2015-05-19 16:06:08 +1000

    Minor changes.

--- conf/update_from_nectar.sh
+++ conf/update_from_nectar.sh
@@ -9,11 +9,23 @@ key='/home/mlake/.ssh/keys/uts-choice.pem'
 # Login details for Nectar instance. 
 src='ec2-user@130.56.248.113'
 
-# Get nginx configuration files from /etc
+#######
+# NGINX
+#######
+
 # Note: we don't get everything from under /etc/nginx/ as many of the files
 # there are just example files for other web applications. 
+
+# Get nginx configuration files. 
 scp -i $key $src:/etc/nginx/nginx.conf nginx/
+scp -i $key $src:/etc/nginx/uwsgi_params nginx/
+
+# Get nginx configuration directories and their contents.
 scp -i $key -r $src:/etc/nginx/conf.d nginx/
+
+#######
+# UWSGI
+#######
 
 # Get uwsgi configuration files from /etc
 scp -i $key $src:/etc/uwsgi.ini uwsgi/
